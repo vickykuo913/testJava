4f24d3699e3ac0eb90366f505712e8383f07b82a
LUNGYEN\vicky.kuo <vicky.kuo@lyls.com.tw>
2025-09-26 14:40:01 +0800

    edit L1003

--- TestJava/L1003/L1003.java
+++ TestJava/L1003/L1003.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class L1003{
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         //List<Integer> results=new ArrayList<>();
